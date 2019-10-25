f312e72c0c723a452d652e641ad64fa3dd1af70f
Hankun Chai <1904721@sudent.uwtsd.ac.uk>
2019-10-25 12:23:37 +0000

    bux fixed by Chai

--- src/main/java/com/mycompany/firstproject/Program.java
+++ src/main/java/com/mycompany/firstproject/Program.java
@@ -5,7 +5,7 @@
  */
 package com.mycompany.firstproject;
 import java.util.*;
-
+import java.util.Scanner;
 /**
  *
  * @author uwtsd-student
@@ -21,24 +21,29 @@ public class Program
         //Your job is to fix the errors and finish them.
         
         //Print "Hello, World" to console
-        System.out.printf(Hello, World)
+        System.out.printf("Hello, World");
         
         //Read two numbers, then print the sum of them
-        System.out.printf("Please enter first number: ");
-        int num1 = Integer.parseInt(stdin.nextline());
+        System.out.printf("Please enter first number: \n");
+        int num1 = Integer.parseInt(stdin.nextLine());
         
-        System.out.printf("Please enter first number: ");
-        int num1 = Integer.parseInt(stin.nextLine());
+        System.out.printf("Please enter second number: \n");
+        int num2 = Integer.parseInt(stdin.nextLine());
         
         int result = num1 + num2;
         
-        System.out.printf("The result is: %d", result);
+        System.out.printf("The result is: %d \n", result);
         
         //Ask the user for their name and age, then tell them how old they will be in 10 years.
         //For example, if the user says that their name is Jane and they are 25, the program should output:
         //"Jane, you will be 35 in 10 years time."
-        
-        
+        System.out.println("please enter your name: ");
+        String name = stdin.nextLine();
+        System.out.println("please enter your current age: ");
+        int age = stdin.nextInt()+10;
+        System.out.println("So "+name+", your age is "+age +" in 10 years time");
+
+
         
     }
 }
